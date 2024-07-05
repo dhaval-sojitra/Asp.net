68a74df0323af2b6e8ebf9fc3da43fe94ce39035
unknown <dhavalsojitra875@gmail.com>
2024-07-05 08:09:42 +0530

    gfds

---
+++ StudentApplication/StudentApplication/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentApplication.Models;
+
+namespace StudentApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            StudentModel s = new StudentModel();
+            s.id = 1;
+            s.name = "Dhaval Sojitra";
+            s.city = "Surat";
+
+            List<StudentModel> list = new List<StudentModel>
+            {
+                new StudentModel { id = 1, name = "Dhaval" ,city = "Surat"},
+                new StudentModel { id = 2, name = "Keval" , city = "Surat" }
+            };
+            ViewBag.model = list;
+            return View();
+        }
+        public ActionResult About()
+        {
+            string[] name = { "Dhaval", "Keval", "Jimit", "Jaydeep", "Divyaraj" };
+            ViewBag.name = name;
+            return View();
+        }
+        public ActionResult Contect()
+        {
+            return View();
+        }
+    }
+}
