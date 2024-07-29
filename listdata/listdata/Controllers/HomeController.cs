8049b7a71dde525468dbe634bb93fa2d126c9713
unknown <dhavalsojitra875@gmail.com>
2024-07-29 08:17:43 +0530

    Added

---
+++ listdata/listdata/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using listdata.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace listdata.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var people = new List<Person>
+            {
+                new Person { FirstName = "Dhaval", LastName = "Sojitra",Age="21" },
+                new Person { FirstName = "Keval", LastName = "Vadadoriya",Age="22" },
+                new Person { FirstName = "Jaydeep", LastName = "Kakadiya",Age="20" },
+                new Person { FirstName = "Divyaraj", LastName = "Ahir",Age="21" },
+                new Person { FirstName = "Jimit", LastName = "Vasoya",Age="21" },
+                new Person { FirstName = "Jay", LastName = "Khunt",Age="22" }
+            };
+
+            return View(people);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ listdata/listdata/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="listdata.MvcApplication" Language="C#" %>
