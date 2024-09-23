fa2e37525954b7133fb05eead8a16b5123643226
unknown <dhavalsojitra875@gmail.com>
2024-09-23 09:16:47 +0530

    Added

---
+++ Login/Login/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Login.Models;
+
+
+namespace Login.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        
+    }
+}
